70cf9065c86fd43213f83e4151bc96e849e4be81
Abhishek Suryawanshi <126605338+Abhi-Suryawanshi@users.noreply.github.com>
2025-04-16 18:28:16 +0530

    Add files via upload

---
+++ JDBC_CRUD/JdbcDemo.java
@@ -0,0 +1,23 @@
+package com.JDBCdemo;
+
+//step 1 - import sql package
+import java.sql.*;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) throws Exception {
+		
+		//step 2 - load driver
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//step 3 - establish connection
+		String url = "jdbc:mysql://localhost:3306/jdbc_1";
+		String uname = "root";
+		String pass = "root";
+		
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		
+		System.out.println("Connection established: " + con);
+	}
+
+}
